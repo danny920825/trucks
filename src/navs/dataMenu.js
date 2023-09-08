4b3dbb9007654bb834a63ceb06f7082c8da8df89
Danny Paula <danny920825@gmail.com>
2023-09-08 08:39:48 -0400

    fisrt commit

---
+++ src/navs/dataMenu.js
@@ -0,0 +1,164 @@
+export const navLinks1 = [
+  {
+    label: 'Track',
+    path: '#',
+  },
+  {
+    label: 'Load Board',
+    path: '#',
+  },
+  {
+    label: 'Investor',
+    path: '#',
+  },
+  {
+    label: 'Blog ',
+    path: '#',
+  }
+]
+
+export const navLinks2 = [
+  {
+    name: 'Our Services',
+    subelements: [
+      {
+        label: 'Dry Van',
+        path: '#',
+      },
+      {
+        label: 'Refrigerated',
+        path: '#',
+      },
+      {
+        label: 'Dedicated',
+        path: '#',
+      },
+      {
+        label: 'Logistics',
+        path: '#',
+      },
+      {
+        label: 'Flatbed',
+        path: '#',
+      },
+      {
+        label: 'Intermodal',
+        path: '#',
+      },
+      {
+        label: 'Heavy Haul',
+        path: '#',
+      },
+      {
+        label: 'Expedited',
+        path: '#',
+      },
+      {
+        label: 'Owner Operator',
+        path: '#',
+      },
+      {
+        label: 'Equipment and Leasing',
+        path: '#',
+      },
+      {
+        label: 'Warehousing',
+        path: '#',
+      },
+      {
+        label: 'Mexico Services',
+        path: '#',
+      },
+    ],
+  },
+  {
+    name: 'Careers',
+    subelements: [
+      {
+        label: 'Dry Van',
+        path: '#',
+      },
+      {
+        label: 'Refrigerated',
+        path: '#',
+      },
+      {
+        label: 'Dedicated',
+        path: '#',
+      },
+      {
+        label: 'Logistics',
+        path: '#',
+      },
+      {
+        label: 'Flatbed',
+        path: '#',
+      },
+      {
+        label: 'Intermodal',
+        path: '#',
+      },
+      {
+        label: 'Heavy Haul',
+        path: '#',
+      },
+      {
+        label: 'Expedited',
+        path: '#',
+      },
+      {
+        label: 'Owner Operator',
+        path: '#',
+      },
+      {
+        label: 'Equipment and Leasing',
+        path: '#',
+      },
+      {
+        label: 'Warehousing',
+        path: '#',
+      },
+      {
+        label: 'Mexico Services',
+        path: '#',
+      },
+    ],
+  },
+  {
+    name: 'Truck Driving School',
+    subelements: [
+      {
+        label: 'CDL Information',
+        path: '#',
+      },
+      {
+        label: 'What Is a CDL (Commercial Drivers License)',
+        path: '#',
+      },
+      {
+        label: 'Class A CDL Information',
+        path: '#',
+      },
+      {
+        label: 'How to Get a CDL',
+        path: '#',
+      },
+      {
+        label: 'CDL License Requirements',
+        path: '#',
+      },
+      {
+        label: 'Types OF CDL Tests',
+        path: '#',
+      },
+      {
+        label: 'How to Study for Your CDL',
+        path: '#',
+      },
+      {
+        label: 'CDL Class A Permit Test Information',
+        path: '#',
+      },
+    ],
+  },
+]
